78a2862590563f4a730d641b1c73bd286c2448e3
MohammadReza <mhmd.samadi76@gmail.com>
2018-07-04 21:50:31 +0430

    initial commit

---
+++ app/src/main/java/com/bluecode/mhmd/cammera_testapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bluecode.mhmd.cammera_testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
